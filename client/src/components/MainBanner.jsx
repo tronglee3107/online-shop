9f36e26a87fe9f63d65f75078c8ccad1283843d0
Le Dao Duy Trong <trong.le@getvymo.com>
2025-04-19 16:50:37 +0700

    adding home banner

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -8,7 +8,7 @@ const MainBanner = () => {
             <img src={assets.main_banner_bg_sm} alt="banner" className="w-full md:hidden"/>
 
             <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
-                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">Buying the whole world on our website!!!</h1>
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">Everything you need, all in one place!</h1>
             
                 <div className="flex items-center mt-6 font-medium">
                     <Link to={"/products"} className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">
