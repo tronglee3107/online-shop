87397a644f14cf06202bd79074fbaa5d4866846c
Le Dao Duy Trong <trong.le@getvymo.com>
2025-04-22 00:50:14 +0700

    adding seller page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,11 +11,16 @@ import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from "./pages/seller/sellerLayout";
+import AddProduct from "./pages/seller/AddProduct";
+import ProductList from "./pages/seller/ProductList";
+import Orders from "./pages/seller/Orders";
 function App() {
     const isSellerPath = useLocation().pathname.includes("seller");
-    const {showUserLogin} = useAppContext();
+    const {showUserLogin, isSeller} = useAppContext();
     return (
-        <div>
+        <div className="text-default min-h-screen text-gray-700 bg-white">
             {isSellerPath ? null : <Navbar />}
             {showUserLogin && <Login />}
             <Toaster />
@@ -28,6 +33,11 @@ function App() {
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/add-address" element={<AddAddress />} />
                     <Route path="/my-orders" element={<MyOrders />} />
+                    <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />} >
+                        <Route index element={isSeller ? <AddProduct /> : null}/>
+                        <Route path="product-list" element={<ProductList />}/>
+                        <Route path="orders" element={<Orders />}/>
+                    </Route>
                     
                 </Routes>
             </div>
