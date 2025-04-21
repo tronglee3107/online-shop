43a7f02a63345f8d6ee3b6064c1f45455b1aeae7
Le Dao Duy Trong <trong.le@getvymo.com>
2025-04-21 12:14:25 +0700

    adding login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
 function App() {
     const isSellerPath = useLocation().pathname.includes("seller");
+    const {showUserLogin} = useAppContext();
     return (
         <div>
             {isSellerPath ? null : <Navbar />}
+            {showUserLogin && <Login />}
             <Toaster />
             <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
                 <Routes>
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { useAppContext } from "../context/AppContext";
+
+function Login() {
+    const [state, setState] = React.useState("login");
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const {showUserLogin, setShowUserLogin, setUser} = useAppContext();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        setUser({
+            email: "admintest@gmail.com",
+            name: "admin"
+        })
+        setShowUserLogin(false);
+    }
+    return (
+        <div onClick={() => setShowUserLogin(false)} className="fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50">
+            <form onClick={(e) => e.stopPropagation()} onSubmit={submitHandler} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
+                <p className="text-2xl font-medium m-auto">
+                    <span className="text-primary">User</span>{" "}
+                    {state === "login" ? "Login" : "Sign Up"}
+                </p>
+                {state === "register" && (
+                    <div className="w-full">
+                        <p>Name</p>
+                        <input
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                            placeholder="Type here"
+                            className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+                            type="text"
+                            required
+                        />
+                    </div>
+                )}
+                <div className="w-full ">
+                    <p>Email</p>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        placeholder="Type here"
+                        className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+                        type="email"
+                        required
+                    />
+                </div>
+                <div className="w-full ">
+                    <p>Password</p>
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        placeholder="Type here"
+                        className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
+                        type="password"
+                        required
+                    />
+                </div>
+                {state === "register" ? (
+                    <p>
+                        Already have account?{" "}
+                        <span
+                            onClick={() => setState("login")}
+                            className="primary cursor-pointer text-primary">
+                            Click here
+                        </span>
+                    </p>
+                ) : (
+                    <p>
+                        Create an account?{" "}
+                        <span
+                            onClick={() => setState("register")}
+                            className="text-primary cursor-pointer text-primary">
+                            Click here
+                        </span>
+                    </p>
+                )}
+                <button className="bg-primary hover:bg-primary-dull transition-all text-white w-full py-2 rounded-md cursor-pointer">
+                    {state === "register" ? "Create Account" : "Login"}
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
