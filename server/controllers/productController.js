8f7c4e3710333b80c78014e66d540406657d5852
Trong <tronglee3107>
2025-04-26 22:41:43 +0700

    adding product api

---
+++ server/controllers/productController.js
@@ -0,0 +1,27 @@
+
+
+// /api/product/add
+export const addProduct = async (req, res) => {
+    
+}
+
+// /api/product/list
+export const getAllProduct = async (req, res) => {
+
+}
+
+// /api/product/:id
+export const getProductById = async (req, res) => {
+
+}
+
+
+// update product stock --> /api/product/stock
+export const changeProductStock = async (req, res) => {
+
+}
+
+// /api/product/delete
+export const deleteProduct = async (req, res) => {
+
+}
