6af79bb483d4205bd713161ad7b789ffd61f37f6
Trong <--add>
2025-04-28 17:34:22 +0700

    remove middleware

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,9 +4,9 @@ import { addProduct, changeProductStock, getAllProduct, getProductById } from '.
 import { authAdmin } from '../middlewares/authAdmin.js';
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.array(["images"]), authAdmin, addProduct);
-productRouter.get('/list', authAdmin, getAllProduct);
-productRouter.get('/id', authAdmin, getProductById);
-productRouter.post('/stock', authAdmin, changeProductStock);
+productRouter.post('/add', upload.array(["images"]), addProduct);
+productRouter.get('/list', getAllProduct);
+productRouter.get('/id', getProductById);
+productRouter.post('/stock', changeProductStock);
 
 export default productRouter;
