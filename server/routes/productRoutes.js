12f33321cc95cbb36e865bf5142142f00cf80215
Trong <--add>
2025-04-28 11:19:37 +0700

    fix product middleware

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,7 +4,7 @@ import { addProduct, changeProductStock, getAllProduct, getProductById } from '.
 import { authAdmin } from '../middlewares/authAdmin.js';
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.array([]), authAdmin, addProduct);
+productRouter.post('/add', upload.array(["images"]), authAdmin, addProduct);
 productRouter.get('/list', authAdmin, getAllProduct);
 productRouter.get('/id', authAdmin, getProductById);
 productRouter.post('/stock', authAdmin, changeProductStock);
